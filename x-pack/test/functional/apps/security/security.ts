82e80970d4c89af45bd9159a0593de9ba01a6ab1
Brad Jones <brad.jones@eighttwelve.co>
2023-12-08 11:33:12 -0700

    Updated User Profile with Derby Styles

--- x-pack/test/functional/apps/security/security.ts
+++ x-pack/test/functional/apps/security/security.ts
@@ -48,7 +48,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         await PageObjects.security.logout();
 
         const logoutMessage = await testSubjects.getVisibleText('loginInfoMessage');
-        expect(logoutMessage).to.eql('You have logged out of Elastic.');
+        expect(logoutMessage).to.eql('You have logged out of PRISM.');
       });
 
       describe('within a non-default space', async () => {
